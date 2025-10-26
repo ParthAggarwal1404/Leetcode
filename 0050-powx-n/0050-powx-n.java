a3af967f4f438ebe2e5de6b62bc3572d45d469fa
Parth Aggarwal <141123480+ParthAggarwal1404@users.noreply.github.com>
2025-10-26 22:40:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (68.52%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans = Math.pow(x,n);
-        return ans;
+        if(n==0){
+            return 1.00;
+        }
+        else if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double result=1;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                result *= x;
+            }
+            x *= x;
+            n >>>= 1;
+        }
+        return result;
     }
 }
