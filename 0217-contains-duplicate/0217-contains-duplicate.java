be3be37d930c76e0b30317bac1d3503a8dd4b866
Parth Aggarwal <141123480+ParthAggarwal1404@users.noreply.github.com>
2025-12-23 18:58:27 +0530

    Time: 39 ms (5.44%), Space: 88.8 MB (62.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        HashMap<Integer , Integer> hp= new HashMap<>();
+        HashMap<Integer, Integer> hp = new HashMap<>();
 
-        for(int i:nums){
+        for(int i: nums){
 
             if(hp.containsKey(i)){
-
-                hp.put(i , hp.get(i) + 1);
-
+                hp.put(i , hp.get(i) +1);
             }
             else{
                 hp.put(i,1);
             }
         }
         for(int key : hp.keySet()){
-            if(hp.get(key)>1){
+            if(hp.get(key)> 1){
                 return true;
             }
-            
         }
         return false;
     }
